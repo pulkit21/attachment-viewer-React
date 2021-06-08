a0542425851ef7450e8df1137bc15c8778a7b54c
Pulkit Agarwal <pulkit.ag02@gmail.com>
2021-06-08 15:46:26 +0530

    Fixed the class naming

--- attachment-viewer/src/components/loader/Loader.js
+++ attachment-viewer/src/components/loader/Loader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Loader.css';
 
-const Loader = () => <div class="lds-ring"><div></div><div></div><div></div><div></div></div>;
+const Loader = () => <div className="lds-ring"><div></div><div></div><div></div><div></div></div>;
 
 export default Loader;
